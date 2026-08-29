5385737eb7651e7264b08e638c97a3d72171bddc
league <league@192.168.1.75>
2026-08-29 12:53:07 -0700

    bob

--- src/_01_array_list_visualizer/VisualArrayLists.java
+++ src/_01_array_list_visualizer/VisualArrayLists.java
@@ -21,20 +21,36 @@ public class VisualArrayLists implements ActionListener {
 		list.add("silver");
 		list.add("gold");
 		list.add("diamond");
+		list.set(0, "bob");
 		// 2. Set one item in our list to be something different
 		
 		// 3. Remove something from our list
-		
+		list.remove(1);
 		// 4. Print the size of our list
-		
+	System.out.println(list.size());
 		// 5. Check if our list contains a specific thing of your choice
 		//	  and print whether it is in the list or not.
+		if(list.contains("bob")) {
+			System.out.println("bob is in the list");
+		}
+		if(list.contains("silver")) {
+			System.out.println("silver is in the list");
+		}
+		if(list.contains("gold")) {
+			System.out.println("gold is in the list");
+		}
+		if(list.contains("diamond")) {
+			System.out.println("diamond is in the list");
+		}else {
+			System.out.println("the thing you put is not in the list");
+		}
 		
 		// 6. Get a specific item from our list and print it out
-		
+		System.out.println(list.get(3));
 		// 7. Clear our list of all items
-		
+		list.clear();
 		// 8. Print the size of our list again
+		System.out.println(list.size());
 	}
 	
 	
--- src/_02_array_list_guestbook/GuestBook.java
+++ src/_02_array_list_guestbook/GuestBook.java
@@ -1,6 +1,17 @@
 package _02_array_list_guestbook;
 
-public class GuestBook {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+
+public class GuestBook implements ActionListener {
 	// Create a GUI with two buttons. One button reads "Add Name" and the other button reads "View Names". 
 	// When the add name button is clicked, display an input dialog that asks the user to enter a name. Add
 	// that name to an ArrayList. When the "View Names" button is clicked, display a message dialog that displays
@@ -9,4 +20,49 @@ public class GuestBook {
 	// Guest #2: Sandy Summers
 	// Guest #3: Greg Ganders
 	// Guest #4: Donny Doners
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JButton button1 = new JButton("Add Name");
+	JButton button2 = new JButton("View Name");
+	
+	ArrayList<String> names = new ArrayList<String>();
+	
+	
+	
+	public static void main(String[] args) {
+		GuestBook gb = new GuestBook();
+		gb.start();
+	}
+
+	private void start() {
+		// TODO Auto-generated method stub
+		
+		
+		frame.add(panel);
+		panel.add(button1);
+		panel.add(button2);
+		button1.addActionListener(this);
+		button2.addActionListener(this);
+		frame.pack();
+		frame.setVisible(true);
+		
+		
+		
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		JButton buttonPressed = (JButton) e.getSource();
+		
+		if(buttonPressed == button1 ) {
+			String name = JOptionPane.showInputDialog("Add a name into the guestbook");
+			names.add(name);
+			if(buttonPressed == button2) {
+				JOptionPane.showMessageDialog(null, name);
+			}
+		}
+	}
+	
 }
